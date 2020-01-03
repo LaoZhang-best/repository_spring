14e7dae8eda601edf6b44111c3c08e2332d78a76
Administrator <Administrator@JQJLJDG6B4V3H09>
2020-01-03 14:13:12 +0800

    3

--- _test/src/_test/Hello.java
+++ _test/src/_test/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("我在客户端改了");
 		System.out.println("我在浏览器改了");
+		int i= 10;
+		System.out.println("i");
 	}
 }
--- _test/src/_test/Word.java
+++ _test/src/_test/Word.java
@@ -4,6 +4,6 @@ public class Word{
     public static void main(String[] args){
       System.out.print("hellow");
       System.out.print("第一次修改");
-    
+      System.out.println("第一次改弟弟");
     }
 }
